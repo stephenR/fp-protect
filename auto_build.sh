4ebedb0ddc99df24f83e3b066cae88a5b6370b93
Stephen Röttger <stephen.roettger@zero-entropy.de>
2013-06-18 15:25:52 +0200

    -d to test if directory exists

--- auto_build.sh
+++ auto_build.sh
@@ -15,11 +15,11 @@ setup_env () {
 
 setup_dirs () {
 	echo "[*] Setting up system directories"
-	if [ -f $LFS/tools ]; then
+	if [ -d $LFS/tools ]; then
 		rm -Rf $LFS/tools
 	fi
 	mkdir -p $LFS/tools
-	if [ ! -f /tools ]; then
+	if [ ! -d /tools ]; then
 		sudo ln -sv $LFS/tools /
 	fi
 }
@@ -27,14 +27,14 @@ setup_dirs () {
 build_binutils () {
 	echo "[*] binutils build process started"
 
-	if [ ! -f binutils-2.23.2 ]; then
+	if [ ! -d binutils-2.23.2 ]; then
 		echo "[*] Downloading binutils"
 		wget http://ftp.gnu.org/gnu/binutils/binutils-2.23.2.tar.gz || exit 1
 		tar -xf binutils-2.23.2.tar.gz || exit 1
 		rm binutils-2.23.2.tar.gz
 	fi
 
-	if [ -f binutils-build ]; then
+	if [ -d binutils-build ]; then
 		echo "[*] binutils"
 		rm -Rf binutils-build
 	fi

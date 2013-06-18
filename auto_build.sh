dbddbc79badbe41a2da59942b6360ce3c26f017f
Stephen Röttger <stephen.roettger@zero-entropy.de>
2013-06-18 15:24:13 +0200

    progress output

--- auto_build.sh
+++ auto_build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+DEBUG=1
+
 setup_env () {
+	echo "[*] Setting up environment"
 	export LFS=/home/tsuro/workspace/msc/opt
 	export LC_ALL=POSIX
 	export LFS_TGT=x86_64-lfs-linux-gnu
 	export PATH=/tools/bin:/bin:/usr/bin
-	export MAKEFLAGS='-j 2'
+	if [ ! $DEBUG ]; then
+		export MAKEFLAGS='-j 2'
+	fi
 }
 
 setup_dirs () {
+	echo "[*] Setting up system directories"
 	if [ -f $LFS/tools ]; then
 		rm -Rf $LFS/tools
 	fi
@@ -19,27 +25,34 @@ setup_dirs () {
 }
 
 build_binutils () {
+	echo "[*] binutils build process started"
 
 	if [ ! -f binutils-2.23.2 ]; then
+		echo "[*] Downloading binutils"
 		wget http://ftp.gnu.org/gnu/binutils/binutils-2.23.2.tar.gz || exit 1
 		tar -xf binutils-2.23.2.tar.gz || exit 1
 		rm binutils-2.23.2.tar.gz
 	fi
 
 	if [ -f binutils-build ]; then
+		echo "[*] binutils"
 		rm -Rf binutils-build
 	fi
 
 	mkdir binutils-build
 	cd binutils-build
 
+	echo "[*] Configuring"
 	#../binutils-2.23.2/configure --with-lib-path=/tools/lib --disable-werror
 	../binutils-2.23.2/configure --prefix=/tools --with-sysroot=$LFS --with-lib-path=/tools/lib --disable-nls --disable-werror || exit 1
-	make || exit 1
+	echo "[*] Compiling"
+	make $MAKEFLAGS || exit 1
 	case $(uname -m) in
 		x86_64) mkdir -v /tools/lib && ln -sv lib /tools/lib64 ;;
 	esac
-	make install
+	echo "[*] Installing"
+	make $MAKEFLAGS install
+	echo "[*] binutils build process finished"
 }
 
 setup_env
